74586eb4496c4206a4b5b8ca477517c429a51af3
Soma Stories <info@somastories.app>
2026-03-18 21:06:33 +0300

    v2-add frontend and backend folders

---
+++ frontend/Home.js
@@ -0,0 +1,6 @@
+console.log("Welcome to the Home Page!");
+
+function displayWelcomeMessage() {
+    const welcomeMessage = document.getElementById("welcome-message");
+    welcomeMessage.textContent = "Hello, welcome to our website!";
+}
